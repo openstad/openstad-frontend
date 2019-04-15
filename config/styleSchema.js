ddf10232a0d8e7e715022044e3b0d7f3b329dfc6
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-04-15 16:42:33 +0200

    CSS implementation

---
+++ config/styleSchema.js
@@ -0,0 +1,52 @@
+exports.default = {
+  definition: (name, label) => {
+    return {
+      name: name ? name : 'styles',
+      label: label ? label : 'Styles',
+      type: 'array',
+      titleField: 'property',
+      schema: [
+        {
+          name: 'property',
+          type: 'select',
+          label: 'CSS property',
+          choices: [
+            {
+              'label': 'Background color',
+              'value': 'background-color'
+            },
+            {
+              'label': 'Margin',
+              'value': 'margin'
+            },
+            {
+              'label': 'Padding ',
+              'value': 'padding'
+            },
+            {
+              'label': 'Font size',
+              'value': 'font-size'
+            },
+          ]
+        },
+        {
+          name: 'value',
+          type: 'text',
+          label: 'CSS Value',
+          type: 'string',
+        },
+      ],
+    }
+  },
+  format: (cssValues) => {
+    let formattedStyles = '';
+
+    if (cssValues) {
+      cssValues.forEach((css) => {
+        formattedStyles += `${css.property}:${css.value};`;
+      });
+    }
+
+    return formattedStyles;
+  }
+}
