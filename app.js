bee83d1d81ef4d5d068cbe842510c97ead6fa8c8
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-07-16 13:23:44 +0200

    Enable voting on one plan: styling, js & save vote

--- app.js
+++ app.js
@@ -83,9 +83,9 @@ function serveSite(req, res, siteConfig, forceRestart) {
 
   let dbName = siteConfig.config && siteConfig.config.cms && siteConfig.config.cms.dbName ? siteConfig.config.cms.dbName : '';
 
-  console.log('siteConfig', siteConfig);
+//  console.log('siteConfig', siteConfig);
 
-  console.log('dbName', dbName);
+  //console.log('dbName', dbName);
 
   return dbExists(dbName).then((exists) => {
       if (exists || dbName === process.env.DEFAULT_DB)  {
