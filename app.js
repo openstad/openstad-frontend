f5c80163fdc275655536df79a00c9e09f90bb5bc
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-07-30 10:04:41 +0200

    turn off workflow

--- app.js
+++ app.js
@@ -117,296 +117,305 @@ function serveSite(req, res, siteConfig, forceRestart) {
 function run(id, siteData, callback) {
   const options = { }
   const site = { _id: id}
-  const apos = apostrophe(
-    _.merge({
-      //multisite: self,
-      afterListen: function() {
-        apos._id = site._id;
-        if (callback) {
-          return callback(null, apos);
+
+  const siteConfig = {
+  //multisite: self,
+  afterListen: function() {
+    apos._id = site._id;
+    if (callback) {
+      return callback(null, apos);
+    }
+  },
+//    rootDir: getRootDir() + '/sites',
+//    npmRootDir: getRootDir(),
+  //shortName: 'localhost',
+  shortName: site._id,
+  modules: {
+    'api-proxy': {},
+    'apostrophe-db': {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 27017,
+    },
+    'apostrophe-express': {
+      port: process.env.PORT
+    },
+
+  /*  'apostrophe-i18n': {
+         locales: ['nl', 'en'],
+         defaultLocale: 'nl'
+    },*/
+
+/*    'apostrophe-docs': {
+      beforeConstruct: function(self, options) {
+        console.log('==>>>> beforeConstruct', options);
+        options.permission = false;
+      }
+    },*/
+    'auth': {},
+    'apostrophe-multisite-fake-listener': {
+      construct: function(self, options) {
+        // Don't really listen for connections. We'll run as middleware
+        self.apos.listen = function() {
+          if (self.apos.options.afterListen) {
+            return self.apos.options.afterListen(null);
+          }
         }
-      },
-  //    rootDir: getRootDir() + '/sites',
-  //    npmRootDir: getRootDir(),
-      //shortName: 'localhost',
-      shortName: site._id,
-      modules: {
-        'api-proxy': {},
-				'apostrophe-db': {
-					host: process.env.DB_HOST || 'localhost',
-					port: process.env.DB_PORT || 27017,
-				},
-        'apostrophe-express': {
-          port: process.env.PORT
-        },
+      }
+    },
 
-      /*  'apostrophe-i18n': {
-             locales: ['nl', 'en'],
-             defaultLocale: 'nl'
-        },*/
-        'apostrophe-workflow': {
-          // IMPORTANT: if you follow the examples below,
-          // be sure to set this so the templates work
-          alias: 'workflow',
-          // Recommended to save database space. You can still
-          // export explicitly between locales
-          replicateAcrossLocales: true,
+    'apostrophe-multisite-patch-assets': {
+      construct: function(self, options) {
+        // At least one site has already started up, which means
+        // assets have already been attended to. Steal its
+        // asset generation identifier so they don't fight.
+        // We're not too late because apostrophe-assets doesn't
+        // use this information until afterInit
+        const sample = getSampleSite();
+        if (!sample) {
+          return;
+        }
+        self.apos.assets.generation = sample.assets.generation;
+      }
+    },
 
-      /*  locales: [
-            {
-              name: 'default',
-              label: 'Default',
-              private: true,
-              children: [
-                {
-                  name: 'nl',
-                  label: 'Netherlands'
-                },
-                {
-                  name: 'en',
-                  label: 'English'
-                },
-
-              ]
-            },
-          ],*/
-      },
-  //      'apostrophe-workflow-modified-documents': {},
-    /*    'apostrophe-docs': {
-          beforeConstruct: function(self, options) {
-            console.log('==>>>> beforeConstruct', options);
-            options.permission = false;
-          }
-        },*/
-        'auth': {},
-        'apostrophe-multisite-fake-listener': {
-          construct: function(self, options) {
-            // Don't really listen for connections. We'll run as middleware
-            self.apos.listen = function() {
-              if (self.apos.options.afterListen) {
-                return self.apos.options.afterListen(null);
-              }
+    settings: {
+      // So we can write `apos.settings` in a template
+      alias: 'settings',
+      apiUrl: process.env.API,
+      appUrl: process.env.APP_URL,
+      apiLogoutUrl:  process.env.API_LOGOUT_URL,
+      openStadMap: openstadMap,
+      openstadMapPolygons: openstadMapPolygons,
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      siteConfig: siteData,
+      // Let's pass in a Google Analytics id, just as an example
+      contentWidgets: {
+          'agenda' : {},
+          'accordeon': {},
+      /*    'apostrophe-images': {
+            fields: {
+              type: 'string',
+              name: 'maxWidth',
+              label: 'Max width'
             }
-          }
-        },
-
-        'apostrophe-multisite-patch-assets': {
-          construct: function(self, options) {
-            // At least one site has already started up, which means
-            // assets have already been attended to. Steal its
-            // asset generation identifier so they don't fight.
-            // We're not too late because apostrophe-assets doesn't
-            // use this information until afterInit
-            const sample = getSampleSite();
-            if (!sample) {
-              return;
-            }
-            self.apos.assets.generation = sample.assets.generation;
-          }
-        },
-
-        settings: {
-          // So we can write `apos.settings` in a template
-          alias: 'settings',
-          apiUrl: process.env.API,
-          appUrl: process.env.APP_URL,
-          apiLogoutUrl:  process.env.API_LOGOUT_URL,
-          openStadMap: openstadMap,
-          openstadMapPolygons: openstadMapPolygons,
-          googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
-          siteConfig: siteData,
-          // Let's pass in a Google Analytics id, just as an example
-          contentWidgets: {
-              'agenda' : {},
-              'accordeon': {},
-          /*    'apostrophe-images': {
-                fields: {
-                  type: 'string',
-                  name: 'maxWidth',
-                  label: 'Max width'
-                }
-              },*/
-              'arguments' : {},
-              'arguments-form' : {},
-              'section' : {
-                addLabel: 'Columns',
-                controls: {
-                  movable: true,
-                  removable: true,
-                  position: 'bottom-left'
-                },
-              },
-              'counter' : {
-                addLabel: 'Counter',
-              },
-              'date-bar' : {},
-              'idea-form' : {},
-              'idea-map': {},
-              'idea-overview' : {},
-              'idea-single' : {},
-              'image' : {},
-              'link': {},
-              'list' : {},
-              'gebiedsontwikkeling-tool': {
-                addLabel: 'Map for area development',
-              },
-              'begroot': {
-                addLabel: 'Participatory budgetting',
-              },
-              'main-image' : {},
-              'apostrophe-rich-text': {
-                toolbar: [ 'Styles', 'Bold', 'Italic', 'Link', 'Unlink', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', ],
-
-              /*  toolbar : [
-                  { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
-               	  { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
-                ],*/
-                styles: [
-                  { name: 'Heading 1', element: 'h1' },
-                  { name: 'Heading 2', element: 'h2' },
-                  { name: 'Heading 3', element: 'h3' },
-                  { name: 'Heading 4', element: 'h4' },
-                  { name: 'Paragraph', element: 'p' }
-                ],
-                controls: {
-                  movable: true,
-                  removable: true,
-                  position: 'top-right'
-                }
-              },
-
-              'speech-bubble' : {},
-              'title' : {},
-        //      'user-form' : {},
-              'local-video': {
-                addLabel: 'Video (upload)',
-              },
-              'apostrophe-video' : {
-                addLabel: 'Video (3d party, youtube, vimeo, etc.)',
-              },
-
-          }
-        },
-
-        // Apostrophe module configuration
-
-        // Note: most configuration occurs in the respective
-        // modules' directories. See lib/apostrophe-assets/index.js for an example.
-
-        // However any modules that are not present by default in Apostrophe must at
-        // least have a minimal configuration here: `moduleName: {}`
-
-        // If a template is not found somewhere else, serve it from the top-level
-        // `views/` folder of the project
-
-    //    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
-        'idea-pages': {},
-        'apostrophe-pages': {
-          types: [
-            {
-              name: 'default',
-              label: 'Default'
+          },*/
+          'arguments' : {},
+          'arguments-form' : {},
+          'section' : {
+            addLabel: 'Columns',
+            controls: {
+              movable: true,
+              removable: true,
+              position: 'bottom-left'
             },
-            {
-              name: 'idea',
-              label: 'Idea'
-            }
-          ]
-        },
-        'apostrophe-global': {},
-      /*  'one-column-widgets': {},
-        'two-column-widgets': {},
-        'four-column-widgets': {},
-        'three-column-widgets': {},
-        'two-third-column-widgets': {},
-        'spacer-widgets': {},*/
-        'section-widgets': {},
-        'card-widgets': {},
-        'iframe-widgets': {},
-        'speech-bubble-widgets': {},
-        'title-widgets': {},
-        'main-image-widgets': {},
-        'list-widgets': {},
-        'agenda-widgets': {},
-        'accordeon-widgets': {},
-        'idea-overview-widgets': {},
-        'icon-section-widgets': {},
-        'idea-single-widgets': {},
-        'idea-form-widgets': {},
-        'date-bar-widgets': {},
-        'idea-map-widgets': {},
-        'idea-voting-widgets': {},
-        'link-widgets': {},
-        'counter-widgets': {},
-        'arguments-widgets': {},
-        'arguments-form-widgets': {},
-        'gebiedsontwikkeling-tool-widgets': {},
-        'user-form-widgets': {},
-        'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
-        'submissions-widgets': {},
-        'begroot-widgets': {},
-        'local-video-widgets': {},
-        'image-widgets': {},
-        'apostrophe-palette-widgets': {},
-        'apostrophe-palette': {},
-        'apostrophe-video-widgets': {},
-        'apostrophe-palette-global': {
-          paletteFields: [
-            {
-              name: 'backgroundNavColor',
-              label: 'Background color of the navigation bar',
-              type: 'color',
-              selector: '#navbar',
-              property: 'background-color',
-            },
-            {
-              name: 'backgroundFooterColor',
-              label: 'Background color of the footer',
-              type: 'color',
-              selector: 'footer',
-              property: 'background-color',
-            },
-            {
-              name: 'logoWidth',
-              label: 'Logo breedte',
-              type: 'range',
-              selector: ['#logo-image'],
-              property: ['width'],
-              min: 25,
-              max: 300,
-              step: 1,
-              unit: 'px',
-        //      mediaQuery: '(max-width: 59.99em)'
-            },
-          ],
-          arrangePaletteFields: [
-            {
-              name: 'colorFields',
-              label: 'Kleuren',
-              fields: ['backgroundNavColor', 'backgroundFooterColor']
-            },
-            {
-              name: 'logoFields',
-              label: 'Logo instellingen',
-              fields: ['logoWidth']
-            },
-          ]
-        },
-        'apostrophe-assets' : {
-            minify: process.env.MINIFY_JS && (process.env.MINIFY_JS == 1 || process.env.MINIFY_JS === 'ON'),
-            scripts: [
-              { name: 'cookies' },
-              { name: 'site' },
-              { name: 'jquery.validate.min' },
-              { name: 'jquery.validate.nl' },
+          },
+          'counter' : {
+            addLabel: 'Counter',
+          },
+          'date-bar' : {},
+          'idea-form' : {},
+          'idea-map': {},
+          'idea-overview' : {},
+          'idea-single' : {},
+          'image' : {},
+          'link': {},
+          'list' : {},
+          'gebiedsontwikkeling-tool': {
+            addLabel: 'Map for area development',
+          },
+          'begroot': {
+            addLabel: 'Participatory budgetting',
+          },
+          'main-image' : {},
+          'apostrophe-rich-text': {
+            toolbar: [ 'Styles', 'Bold', 'Italic', 'Link', 'Unlink', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', ],
+
+          /*  toolbar : [
+              { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
+              { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+            ],*/
+            styles: [
+              { name: 'Heading 1', element: 'h1' },
+              { name: 'Heading 2', element: 'h2' },
+              { name: 'Heading 3', element: 'h3' },
+              { name: 'Heading 4', element: 'h4' },
+              { name: 'Paragraph', element: 'p' }
             ],
-            stylesheets: [
-              { name: 'main' }
-            ],
-        },
+            controls: {
+              movable: true,
+              removable: true,
+              position: 'top-right'
+            }
+          },
+
+          'speech-bubble' : {},
+          'title' : {},
+    //      'user-form' : {},
+          'local-video': {
+            addLabel: 'Video (upload)',
+          },
+          'apostrophe-video' : {
+            addLabel: 'Video (3d party, youtube, vimeo, etc.)',
+          },
 
       }
-    }, siteData)
+    },
+
+    // Apostrophe module configuration
+
+    // Note: most configuration occurs in the respective
+    // modules' directories. See lib/apostrophe-assets/index.js for an example.
+
+    // However any modules that are not present by default in Apostrophe must at
+    // least have a minimal configuration here: `moduleName: {}`
+
+    // If a template is not found somewhere else, serve it from the top-level
+    // `views/` folder of the project
+
+//    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
+    'idea-pages': {},
+    'apostrophe-pages': {
+      types: [
+        {
+          name: 'default',
+          label: 'Default'
+        },
+        {
+          name: 'idea',
+          label: 'Idea'
+        }
+      ]
+    },
+    'apostrophe-global': {},
+  /*  'one-column-widgets': {},
+    'two-column-widgets': {},
+    'four-column-widgets': {},
+    'three-column-widgets': {},
+    'two-third-column-widgets': {},
+    'spacer-widgets': {},*/
+    'section-widgets': {},
+    'card-widgets': {},
+    'iframe-widgets': {},
+    'speech-bubble-widgets': {},
+    'title-widgets': {},
+    'main-image-widgets': {},
+    'list-widgets': {},
+    'agenda-widgets': {},
+    'accordeon-widgets': {},
+    'idea-overview-widgets': {},
+    'icon-section-widgets': {},
+    'idea-single-widgets': {},
+    'idea-form-widgets': {},
+    'date-bar-widgets': {},
+    'idea-map-widgets': {},
+    'idea-voting-widgets': {},
+    'link-widgets': {},
+    'counter-widgets': {},
+    'arguments-widgets': {},
+    'arguments-form-widgets': {},
+    'gebiedsontwikkeling-tool-widgets': {},
+    'user-form-widgets': {},
+    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
+    'submissions-widgets': {},
+    'begroot-widgets': {},
+    'local-video-widgets': {},
+    'image-widgets': {},
+    'apostrophe-palette-widgets': {},
+    'apostrophe-palette': {},
+    'apostrophe-video-widgets': {},
+    'apostrophe-palette-global': {
+      paletteFields: [
+        {
+          name: 'backgroundNavColor',
+          label: 'Background color of the navigation bar',
+          type: 'color',
+          selector: '#navbar',
+          property: 'background-color',
+        },
+        {
+          name: 'backgroundFooterColor',
+          label: 'Background color of the footer',
+          type: 'color',
+          selector: 'footer',
+          property: 'background-color',
+        },
+        {
+          name: 'logoWidth',
+          label: 'Logo breedte',
+          type: 'range',
+          selector: ['#logo-image'],
+          property: ['width'],
+          min: 25,
+          max: 300,
+          step: 1,
+          unit: 'px',
+    //      mediaQuery: '(max-width: 59.99em)'
+        },
+      ],
+      arrangePaletteFields: [
+        {
+          name: 'colorFields',
+          label: 'Kleuren',
+          fields: ['backgroundNavColor', 'backgroundFooterColor']
+        },
+        {
+          name: 'logoFields',
+          label: 'Logo instellingen',
+          fields: ['logoWidth']
+        },
+      ]
+    },
+    'apostrophe-assets' : {
+        minify: process.env.MINIFY_JS && (process.env.MINIFY_JS == 1 || process.env.MINIFY_JS === 'ON'),
+        scripts: [
+          { name: 'cookies' },
+          { name: 'site' },
+          { name: 'jquery.validate.min' },
+          { name: 'jquery.validate.nl' },
+        ],
+        stylesheets: [
+          { name: 'main' }
+        ],
+    },
+
+  }
+};
+
+
+  if (process.env.APOS_WORKFLOW === 'ON') {
+    siteConfig['apostrophe-workflow'] = {
+      // IMPORTANT: if you follow the examples below,
+      // be sure to set this so the templates work
+      alias: 'workflow',
+      // Recommended to save database space. You can still
+      // export explicitly between locales
+      replicateAcrossLocales: true,
+
+  /*  locales: [
+        {
+          name: 'default',
+          label: 'Default',
+          private: true,
+          children: [
+            {
+              name: 'nl',
+              label: 'Netherlands'
+            },
+            {
+              name: 'en',
+              label: 'English'
+            },
+
+          ]
+        },
+      ],*/
+    };
+
+    siteConfig['apostrophe-workflow-modified-documents'] = {};
+  }
+
+  const apos = apostrophe(
+    _.merge(siteConfig, siteData)
   );
 
 }
