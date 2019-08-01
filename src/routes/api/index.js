fb39735b5ce0045e5d14ab49277c4217b31b9557
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-08-01 11:51:01 +0200

    add json output voor site api

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -21,4 +21,17 @@ router.use( '/site/:siteId(\\d+)/vote', require('./vote') );
 // openstad-map
 router.use( '/site/:siteId(\\d+)/openstad-map', require('./openstad-map') );
 
+// output error as JSON only use this error handler middleware in "/api" based routes
+router.use("/site", function(err, req, res, next){
+  console.log('===> err', err);
+
+  // use the error's status or default to 500
+  res.status(err.status || 500);
+
+  // send back json data
+  res.send({
+    message: err.message
+  })
+});
+
 module.exports = router;
