4c1c0cb1d10eed892570c74b27fc32029f506aed
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-09-02 18:44:46 +0200

    Implementing sorting on begroot

---
+++ config/sorting.js
@@ -0,0 +1,34 @@
+exports.options = [
+  {
+    value: 'createdtime,desc',
+    label: 'Newest first'
+  },
+  {
+    value: 'createdtime,asc',
+    label: 'Oldest first'
+  },
+  {
+    value: 'random',
+    label: 'Random'
+  },
+  {
+    value: 'likes,desc',
+    label: 'Most liked'
+  },
+  {
+    value: 'likes,asc',
+    label: 'Least liked'
+  },
+  {
+    value: 'ranking,desc',
+    label: 'Ranking'
+  },
+  {
+    value: 'budget,desc',
+    label: 'Highest amount'
+  },
+  {
+    value: 'budget,asc',
+    label: 'Lowest amount'
+  },
+];
