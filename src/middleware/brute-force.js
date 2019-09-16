d63f8392815e07db3025edda659623bd79b2d3eb
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-09-16 17:42:16 +0200

    log ip

--- src/middleware/brute-force.js
+++ src/middleware/brute-force.js
@@ -47,6 +47,7 @@ let globalBruteForce = new ExpressBrute(new ExpressBrute.MemoryStore(), {
 });
 
 exports.globalMiddleware = function(req, res, next) {
+  console.log('req.ip', req.ip);
 	if ((config.ignoreBruteForce && config.ignoreBruteForce.indexOf(req.ip) != -1) || ( req.site && req.site.config && req.site.config.ignoreBruteForce && req.site.config.ignoreBruteForce.indexOf(req.ip) != -1 )) {
 		next();
 	} else {
