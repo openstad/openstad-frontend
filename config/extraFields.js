bcf6b2e9cce653f3fe027811c078475efa675ce6
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-09-19 22:59:27 +0200

    Add extra fields modal voor admin

---
+++ config/extraFields.js
@@ -0,0 +1,26 @@
+exports.fields = [
+  {
+    label: 'Title',
+    key: 'title',
+  },
+  {
+    label: 'Phone number',
+    extraData: true,
+    key: 'phone',
+  },
+  {
+    label: 'Firstname',
+    userData: true,
+    key: 'firstName',
+  },
+  {
+    label: 'Lastname',
+    userData: true,
+    key: 'lastName',
+  },
+  {
+    label: 'Email',
+    userData: true,
+    key: 'email',
+  },
+];
