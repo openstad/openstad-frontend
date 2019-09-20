9468a04d2ae0aecd7cad0f690d71abee008091d2
Tosh Koevoets <tosh.koevoets@gmail.com>
2019-09-20 11:20:37 +0200

    HotFix, locales not working online

--- config/sorting.js
+++ config/sorting.js
@@ -1,34 +1,41 @@
 exports.options = [
   {
     value: 'createdtime,desc',
-    label: 'Newest first'
+  //  label: 'Newest first',
+    label: 'Nieuwste eerst'
   },
   {
     value: 'createdtime,asc',
-    label: 'Oldest first'
+//    label: 'Oldest first',
+    label: 'Oudste eerst'
   },
   {
     value: 'random',
-    label: 'Random'
+//    label: 'Random',
+    label: 'Willekeurig'
   },
   {
     value: 'likes,desc',
-    label: 'Most liked'
+    //label: 'Most liked',
+    label: 'Meeste likes'
   },
   {
     value: 'likes,asc',
-    label: 'Least liked'
+  //  label: 'Least liked',
+    label: 'Meeste likes'
   },
   {
     value: 'ranking,desc',
-    label: 'Ranking'
+    label: 'Ranking',
   },
   {
     value: 'budget,desc',
-    label: 'Highest amount'
+//    label: 'Highest amount',
+    label: 'Hoogste bedrag'
   },
   {
     value: 'budget,asc',
-    label: 'Lowest amount'
+//    label: 'Lowest amount',
+    label: 'Laatste bedrag'
   },
 ];
