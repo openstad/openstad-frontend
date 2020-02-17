07824a94faf5d7a98308fe6079be09a7ad818693
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-02-17 13:47:16 +0100

    Created button to clear cache in CMS admin menu

--- events/index.js
+++ events/index.js
@@ -3,7 +3,7 @@ class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
 const clearCache = require('../services/cache').clearCache;
 
-
+myEmitter.on('clearCache',  clearCache);
 myEmitter.on('apiPost', clearCache);
 myEmitter.on('voted', clearCache);
 myEmitter.on('ideaCrud', clearCache);
