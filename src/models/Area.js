810ed1d66df1b419ec47f50ac9f0b84e165ca2a4
Rudi van Hierden <rudi@draad.nu>
2020-05-20 14:43:13 +0200

    Fix indentation on Area model

--- src/models/Area.js
+++ src/models/Area.js
@@ -1,22 +1,22 @@
 module.exports = function( db, sequelize, DataTypes ) {
-	var Area = sequelize.define('area', {
-
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false
-		},
-		
-	  polygon: {
-	    type: DataTypes.GEOMETRY,
+  var Area = sequelize.define('area', {
+    
+    name: {
+      type: DataTypes.STRING,
       allowNull: false
     },
-
-	});
-
-	Area.associate = function( models ) {
-		this.hasMany(models.Site);
-	}
-
-	return Area;
-
+    
+    polygon: {
+      type: DataTypes.GEOMETRY,
+      allowNull: false
+    },
+    
+  });
+  
+  Area.associate = function( models ) {
+    this.hasMany(models.Site);
+  }
+  
+  return Area;
+  
 }
