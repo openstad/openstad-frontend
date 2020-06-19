2e426eeb989a1aa4063871fa3a34459f9a8f90f9
stijnvdvegt <stijnvandervegt@gmail.com>
2020-06-19 13:17:45 +0200

    introduce play method for accerdeon-widgets

--- packages/cms/lib/modules/accordeon-widgets/public/js/accordeon.js
+++ packages/cms/lib/modules/accordeon-widgets/public/js/accordeon.js
@@ -1,29 +1,33 @@
-(function () {
-    $(document).ready(function () {
-        $('.accordeon-item .title').on('click', function () {
-            
-            var $accordeonItem = $(this).closest('.accordeon-item');
-            
-            if ($accordeonItem.hasClass('closed')) {
-                // Close all open items
-                closeItem($('.accordeon-item:not(.closed)'));
-                
-                // Open the clicked item
-                openItem($accordeonItem);
-            } else {
-                // Close all open items
-                closeItem($('.accordeon-item:not(.closed)'));
-            }
-        });
-    });
-    
+apos.define('accordeon-widgets', {
+  extend: 'apostrophe-widgets',
+  construct: function (self, options) {
+    self.play = function ($widget, data, options) {
+
+      $widget.find('.accordeon-item .title').on('click', function () {
+
+        var $accordeonItem = $(this).closest('.accordeon-item');
+
+        if ($accordeonItem.hasClass('closed')) {
+          // Close all open items
+          closeItem($('.accordeon-item:not(.closed)'));
+
+          // Open the clicked item
+          openItem($accordeonItem);
+        } else {
+          // Close all open items
+          closeItem($widget.find('.accordeon-item:not(.closed)'));
+        }
+      });
+    }
+
     function closeItem($el) {
-        $el.addClass('closed').find('.description').css({maxHeight: 0});
+      $el.addClass('closed').find('.description').css({maxHeight: 0});
     }
-    
+
     function openItem($el) {
-        // Get height of description and set it as maxheight on the accordeon item
-        var height = $el.find('.description p').outerHeight();
-        $el.removeClass('closed').find('.description').css({maxHeight: height});
+      // Get height of description and set it as maxheight on the accordeon item
+      var height = $el.find('.description p').outerHeight();
+      $el.removeClass('closed').find('.description').css({maxHeight: height});
     }
-})();
+  }
+});
