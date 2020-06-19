34819beca1e0f4fc6a55c22341286fb266b6ba5b
mitkoevoets <mitkoevoets@vaerens.com>
2020-06-19 20:18:54 +0200

    Add

--- src/routes/api/area.js
+++ src/routes/api/area.js
@@ -43,6 +43,92 @@ router.route('/')
       .then(function(result) {
         res.json({ success: true, id: result.id });
       });
+  })
+
+  // update area
+  // -----------
+  .delete(auth.can('area:create'))
+  .put(function(req, res, next) {
+
+    const user = req.results;
+    if (!( user && user.can && user.can('update') )) return next( new Error('You cannot update this User') );
+
+    // todo: dit was de filterbody function, en dat kan nu via de auth functies, maar die is nog instance based
+    let data = {}
+
+    const keys = [ 'firstName', 'lastName', 'email', 'phoneNumber', 'streetName', 'houseNumber', 'city', 'suffix', 'postcode'];
+    keys.forEach((key) => {
+      if (req.body[key]) {
+        data[key] = req.body[key];
+      }
+    });
+
+    const userId = parseInt(req.params.userId) || 1;
+
+    /**
+     * Update the user API first
+     */
+    let which = req.query.useOauth || 'default';
+    let siteOauthConfig = ( req.site && req.site.config && req.site.config.oauth && req.site.config.oauth[which] ) || {};
+    let authServerUrl = siteOauthConfig['auth-server-url'] || config.authorization['auth-server-url'];
+    let authUpdateUrl = authServerUrl + '/api/admin/user/' + req.results.externalUserId;
+    let authClientId = siteOauthConfig['auth-client-id'] || config.authorization['auth-client-id'];
+    let authClientSecret = siteOauthConfig['auth-client-secret'] || config.authorization['auth-client-secret'];
+
+    const apiCredentials = {
+      client_id: authClientId,
+      client_secret: authClientSecret,
+    }
+    const options = {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+      body: JSON.stringify(Object.assign(apiCredentials, data))
+    }
+
+    fetch(authUpdateUrl, options)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+
+        throw createError('Updaten niet gelukt', response);
+      })
+      .then((json) => {
+        //update values from API
+        return db.User
+          .authorizeData(data, 'update')
+          .update(data, {where : { externalUserId: json.id }});
+      })
+      .then( (result) => {
+        return db.User
+          .findOne({
+            where: { id: userId, siteId: req.params.siteId }
+            //where: { id: parseInt(req.params.userId) }
+          })
+      })
+      .then(found => {
+        if ( !found ) throw new Error('User not found');
+        res.json(found);
+      })
+      .catch(err => {
+        console.log(err);
+        return next(err);
+      });
+  })
+
+  // delete area
+  // ---------
+  .delete(auth.can('area:delete'))
+  .delete(function(req, res, next) {
+    req.results
+      .destroy()
+      .then(() => {
+        res.json({ 'area': 'deleted' });
+      })
+      .catch(next);
   });
 
 module.exports = router;
