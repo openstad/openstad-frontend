e09b8149542095ceac7cd75249f6349929e3d54f
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-06-20 15:35:14 +0200

    space change to trigger build test

--- src/routes/doc.js
+++ src/routes/doc.js
@@ -19,6 +19,7 @@ router.route('^/$|^/doc/:which$')
 		}
 		let readme = fs.readFileSync(`doc/${which}.md`).toString();
 		let html = marked( readme );
+
   //  res.end(html);
 
 		res.end(`<!DOCTYPE html>
