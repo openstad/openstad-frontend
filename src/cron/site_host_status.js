ebbf1ef4f06d775c23080e30ea011116a36c49ee
stijnvdvegt <stijnvandervegt@gmail.com>
2020-06-26 08:33:46 +0200

    catch errors and add todo's

--- src/cron/site_host_status.js
+++ src/cron/site_host_status.js
@@ -4,7 +4,7 @@ const db = require('../db');
 const k8s = require('@kubernetes/client-node');
 
 const getK8sApi = () => {
-  const kc = new k8s.KubeConfig()
+  const kc = new k8s.KubeConfig();
   kc.loadFromCluster();
   return kc.makeApiClient(k8s.NetworkingV1beta1Api);
 };
@@ -28,45 +28,70 @@ const getDomainIp = async (domain) => {
   }
 };
 
+const getIngress = async (k8sApi, name, namespace) => {
+  try {
+    return await k8sApi.readNamespacedIngress(name, namespace);
+  } catch(error) {
+    //Todo: log something
+    console.log(error);
+
+    return null;
+  }
+};
+
+const createIngress = async (k8sApi, name, domain, namespace) => {
+  return k8sApi.createNamespacedIngress(namespace, {
+    apiVersions: 'networking.k8s.io/v1beta1',
+    kind: 'Ingress',
+    metadata: {name: `${name}-custom-domain`},
+    spec: {
+      rules: [{
+        host: site.domain,
+        http: {
+          paths: [{
+            backend: {
+              serviceName: 'openstad-frontend-service',
+              servicePort: 4444
+            },
+            path: '/'
+          }]
+        }
+      }],
+      tls: [{hosts: [domain]}]
+    }
+  });
+}
+
 const checkHostStatus = async () => {
 
   const namespace = 'default'; //Todo: get this from env variable
 
   const sites = await db.Site.findAll();
-
   const k8sApi = getK8sApi();
 
   const promises = sites.map(async (site) => {
+    // Todo: skip the sites with config.host.status === true?
+
     const config = site.config;
 
     const domainIp = getDomainIp(site.domain);
     const loadbalancerIp = 1; //Todo: get loadBalancerIp form kubenetes/client
 
-    const ingress = await k8sApi.readNamespacedIngress(site.name, namespace);
+    const ingress = getIngress(k8sApi, site.name, namespace);
 
     if(domainIp !== null && domainIp === loadbalancerIp) {
       // Todo: check if ingress settings are correct?
       if(!ingress) {
-        const response = await k8sApi.createNamespacedIngress(namespace, {
-          apiVersions: 'networking.k8s.io/v1beta1',
-          kind: 'Ingress',
-          metadata: {name: `${site.name}-custom-domain`},
-          spec: {
-            rules: [{
-              host: site.domain,
-              http: {
-                paths: [{
-                  backend: {
-                    serviceName: 'openstad-frontend-service',
-                    servicePort: 4444
-                  },
-                  path: '/'
-                }]
-              }
-            }],
-            tls: [{hosts: [site.domain]}]
-          }
-        });
+        try {
+          const response = await createIngress(k8sApi, site.name, site.domain, namespace);
+        } catch(error) {
+          console.error(error);
+          config.host = {
+            status: true
+          };
+          return await site.update({config});
+        }
+
       }
 
       // update site config with status = true
@@ -76,21 +101,29 @@ const checkHostStatus = async () => {
       return await site.update({config});
     }
 
+    // Check if ingress exists and remove it because domain ip does not match loadbalancer ip
     if(ingress) {
-      await k8sApi.deleteNamespacedIngress(site.name, namespace)
+      try {
+        await k8sApi.deleteNamespacedIngress(site.name, namespace)
+      } catch(error) {
+        console.error(error);
+      }
     }
 
     config.host = {
       status: false
     };
-    return await site.update({config});
 
+    return await site.update({config});
   });
 
   await Promise.all(promises);
 
   // Todo: some output?
+  console.log('all sites checked');
 };
+
+// Todo: remove this method when done debugging.
 checkHostStatus();
 
 // Purpose
