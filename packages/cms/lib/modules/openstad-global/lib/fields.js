daed8ce730e3fb6e6f248f5d8f4f94a50eb0c6b9
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-07-07 14:56:50 +0200

    only format logo if it exists

--- packages/cms/lib/modules/openstad-global/lib/fields.js
+++ packages/cms/lib/modules/openstad-global/lib/fields.js
@@ -176,13 +176,13 @@ module.exports = [
         type: 'string',
         label: 'Formatted Logo',
         formatField: function (value, apos, doc, req) {
+          if ()
           const thisHost = req.headers['x-forwarded-host'] || req.get('host');
           const protocol = req.headers['x-forwarded-proto'] || req.protocol;
           const siteUrl = protocol + '://' + thisHost;
            //const siteUrl = '';
 
-          const formattedUrl =  siteUrl + apos.attachments.url(doc.siteLogo);
-          return formattedUrl;
+          return  doc.siteLogo ? siteUrl + apos.attachments.url(doc.siteLogo) : '';
         },
         apiSyncField: 'styling.logo',
     },
