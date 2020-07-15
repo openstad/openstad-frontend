f44eb5822a70b3ba1ab17dc220ae647bebd0a94b
Niels Vegter <niels@denes.nl>
2020-07-15 13:36:49 +0200

    Add release fixes to dev

--- packages/cms/lib/modules/counter-widgets/index.js
+++ packages/cms/lib/modules/counter-widgets/index.js
@@ -8,40 +8,36 @@ module.exports = {
       name: 'label',
       type: 'string',
       label: 'Label',
-      required: true,
-      help: 'The label shown next to the counter number.',
-      def: 'plannen'
+      required: true
     },
     {
       name: 'url',
       type: 'string',
       label: 'Url',
-      help: 'URL to visit when the counter is clicked (optional).',
       required: false
     },
     {
       name: 'counterType',
       type: 'select',
-      label: 'Select counter type',
-      help: `The idea counter dynamically shows the number of uploaded ideas. The number for the static counter can be set by hand.`,
+      label: 'Select dynamic counter',
       choices: [
         {
-          label: 'Amount of submitted ideas',
+          label: 'Idea count',
           value: 'ideasCount',
         },
         {
-          label: 'Amount of votes',
+          label: 'Vote count',
           value: 'voteCount',
 					showFields: [
 						'voteOpinion'
 					]
         },
         {
-          label: 'Amount of users that have voted',
+          label: 'Voted user count',
           value: 'votedUserCount',
         },
         {
-          label: 'Static counter',
+          label: 'Static count',
           value: 'staticCount',
 					showFields: [
 						'staticCount'
@@ -57,24 +53,24 @@ module.exports = {
     {
       name: 'staticCount',
       type: 'string',
-      label: 'Static counter number',
+      label: 'Static count',
       required: false
     },
     {
       name: 'voteOpinion',
       type: 'select',
-      label: 'Which votes to count?',
+      label: 'opinion',
       choices: [
         {
-          label: 'All votes',
+          label: 'Alles',
           value: '',
         },
         {
-          label: 'Only likes / in favor / positive votes',
+          label: 'Ja/Voor',
           value: 'yes',
         },
         {
-          label: 'Only dislikes / against / negative votes',
+          label: 'Nee/tegen',
           value: 'no',
         },
       ],
@@ -83,22 +79,42 @@ module.exports = {
     styleSchema.definition('containerStyles', 'Styles for the container')
   ],
   construct: function(self, options) {
-    options.arrangeFields = (options.arrangeFields || []).concat([
-      {
-        name: 'general',
-        label: 'General',
-        fields: ['label', 'url', 'counterType', 'staticCount', 'voteOpinion']
-      },
-      {
-        name: 'stylinggroup',
-        label: 'Styling',
-        fields: ['containerStyles']
-      }
-    ]);
 
-    self.getCount = function(widget) {
-      let count;
+    const superLoad = self.load;
+    self.load = function (req, widgets, callback) {
+        widgets.forEach((widget) => {
+          if (widget.containerStyles) {
+            const containerId = styleSchema.generateId();
+            widget.containerId = containerId;
+            widget.formattedContainerStyles = styleSchema.format(containerId, widget.containerStyles);
+          }
+          widget.siteId = req.data.global.siteId;
+
+        });
+
+        return superLoad(req, widgets, function (err) {
+            if (err) {
+                return callback(err);
+            }
+            // `widgets` is each widget of this type being loaded on a page
+            widgets.forEach(function (widget) {
+                widget.ideasCount = req.data.ideas ? req.data.ideas.length : false;
+            });
+            return callback(null);
+        });
+    };
+
+
+
+    const superOutput = self.output;
+    self.output = function(widget, options) {
+      var count;
+
+  //    console.log('----idget.counterTypes', widget);
+
+
       switch(widget.counterType) {
+
         case 'ideasCount':
           if (widget.ideasCount) {
             count = widget.ideasCount;
@@ -127,40 +143,14 @@ module.exports = {
 
         default:
           count = 0;
+
       }
 
-      return ('000' + count).slice(-3);
-    };
+      let len = count.toString().length;
+      widget.count = ('000' + count).slice(-len);
 
-    const superLoad = self.load;
-    self.load = function (req, widgets, callback) {
-        widgets.forEach((widget) => {
-          if (widget.containerStyles) {
-            const containerId = styleSchema.generateId();
-            widget.containerId = containerId;
-            widget.formattedContainerStyles = styleSchema.format(containerId, widget.containerStyles);
-          }
-          widget.siteId = req.data.global.siteId;
-
-        });
-
-        return superLoad(req, widgets, function (err) {
-            if (err) {
-                return callback(err);
-            }
-            // `widgets` is each widget of this type being loaded on a page
-            widgets.forEach(function (widget) {
-                widget.ideasCount = req.data.ideas ? req.data.ideas.length : false;
-            });
-            return callback(null);
-        });
-    };
-
-    const superOutput = self.output;
-    self.output = function(widget, options) {
-      widget.count = self.getCount(widget);
-
-      return superOutput(widget, options);
+      var result = superOutput(widget, options);
+      return result;
     };
 
     const superPushAssets = self.pushAssets;
