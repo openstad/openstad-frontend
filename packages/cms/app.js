cfeadca5fe93f842bb1da3b462c7fd8fa4d2c0d3
stijnvdvegt <stijnvandervegt@gmail.com>
2020-08-12 11:22:28 +0200

    debug database name

--- packages/cms/app.js
+++ packages/cms/app.js
@@ -97,6 +97,8 @@ function serveSite(req, res, siteConfig, forceRestart) {
   const runner = Promise.promisify(run);
   let dbName = siteConfig.config && siteConfig.config.cms && siteConfig.config.cms.dbName ? siteConfig.config.cms.dbName : '';
 
+  console.log('SiteConfig: ', siteConfig.config);
+  console.log('Mongo database name: ', dbName);
   // check if the mongodb database exist. The name for databse
   return dbExists(dbName).then((exists) => {
       // if default DB is set
