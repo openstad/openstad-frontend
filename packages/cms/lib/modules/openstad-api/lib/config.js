d4554c7c80c2cfae48cc3324ad06ccf3f5f74e72
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-08-14 17:17:58 +0200

    check if apiSyncfields is set

--- packages/cms/lib/modules/openstad-api/lib/config.js
+++ packages/cms/lib/modules/openstad-api/lib/config.js
@@ -9,17 +9,19 @@ module.exports = (self, options) => {
 
   // Todo: move this to the global config logic module
   self.syncApiFields = (item, apiSyncFields, siteConfig, workflowLocale, body) => {
-    apiSyncFields.forEach(field => {
-      let value = self.getApiConfigValue(siteConfig, field.apiSyncField);
+    if (apiSyncFields) {
+      apiSyncFields.forEach(field => {
+        let value = self.getApiConfigValue(siteConfig, field.apiSyncField);
 
-      if (workflowLocale && workflowLocale === 'default-draft') {
-        value = typeof item[field.name] !== 'undefined' ? item[field.name] : value;
-      } else if(body && body.data && body.data[field.name]) {
-        value = self.getFieldValue(body.data, field);
-      }
+        if (workflowLocale && workflowLocale === 'default-draft') {
+          value = typeof item[field.name] !== 'undefined' ? item[field.name] : value;
+        } else if(body && body.data && body.data[field.name]) {
+          value = self.getFieldValue(body.data, field);
+        }
 
-      item[field.name] = value;
-    });
+        item[field.name] = value;
+      });
+    }
 
     return item;
   };
