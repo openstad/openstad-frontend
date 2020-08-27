35ab4a7a5408225d5a3504fa26f5fc1d7b948fea
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-08-27 20:51:16 +0200

    console.log urls

--- packages/cms/lib/modules/openstad-auth/index.js
+++ packages/cms/lib/modules/openstad-auth/index.js
@@ -29,6 +29,12 @@ module.exports = {
     self.expressMiddleware = {
       when: 'afterRequired',
       middleware: (req, res, next) => {
+        var url      = req.originalUrl;
+    		var method   = req.method;
+    		var userId   = req.user && req.user.id;
+    		var userRole = req.user && req.user.role;
+    		console.log(`${method} "${url}" ${userRole}(${userId})`);
+
         self.authenticate(req, res, next);
       }
     };
