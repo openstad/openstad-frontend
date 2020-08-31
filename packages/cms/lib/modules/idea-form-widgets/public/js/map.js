80a6d769f0207d4d22786ea3393ce4defbfd39dc
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-08-31 10:30:11 +0200

    improved undefined check

--- packages/cms/lib/modules/idea-form-widgets/public/js/map.js
+++ packages/cms/lib/modules/idea-form-widgets/public/js/map.js
@@ -2,7 +2,7 @@ apos.define('idea-form-widgets', {
     extend: 'map-widgets',
     construct: function(self, options) {
         self.play = function($widget, data, options) {
-            var mapConfig = resourceMapConfig ? resourceMapConfig : {};
+            var mapConfig = typeof resourceMapConfig !== 'undefined' && resourceMapConfig ? resourceMapConfig : {};
             self.createMap(mapConfig);
             self.addPolygon(mapConfig);
             self.setIdeaMarker(mapConfig);
--- packages/cms/lib/modules/idea-single-widgets/public/js/main.js
+++ packages/cms/lib/modules/idea-single-widgets/public/js/main.js
@@ -2,7 +2,7 @@ apos.define('idea-single-widgets', {
     extend: 'map-widgets',
     construct: function(self, options) {
         self.play = function($widget, data, options) {
-            var mapConfig = resourceMapConfig && resourceMapConfig ? resourceMapConfig : {};
+            var mapConfig = typeof resourceMapConfig !== 'undefined' && resourceMapConfig ? resourceMapConfig : {};
             var map = self.createMap(mapConfig);
 
             self.addPolygon(mapConfig);
