cdc5edfe5d84331071c9b0cc60e900921482eb85
Niels Vegter <niels@denes.nl>
2020-09-03 15:40:16 +0200

    Clearer field name description

--- packages/cms/lib/modules/ideas-on-map-widgets/index.js
+++ packages/cms/lib/modules/ideas-on-map-widgets/index.js
@@ -205,7 +205,8 @@ module.exports = {
         {
           type:  'string',
           name:  'name',
-          label: 'Name'
+          label: 'Name of the database field',
+			    required: true,
         },
         {
           type:  'string',
