c66fe7abe3410c09917b19bf3e478ecbf2c7876b
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-09-11 10:56:14 +0200

    remove filter

--- src/middleware/filtering.js
+++ src/middleware/filtering.js
@@ -3,8 +3,6 @@ const isJson = require('../util/isJson');
 module.exports = function(req, res, next) {
   const { filter } = req.query;
 
-  console.log(JSON.parse(filter));
-
   if (filter && isJson(filter)) {
     req.dbQuery.where = [JSON.parse(filter)];
   }
