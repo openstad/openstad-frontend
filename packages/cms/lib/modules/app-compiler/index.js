9f575a05e18b483b6dac4032a08ada8964da9789
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-09-26 12:11:38 +0200

    Setting up parsing

--- packages/cms/lib/modules/app-compiler/index.js
+++ packages/cms/lib/modules/app-compiler/index.js
@@ -1,9 +1,9 @@
 const proxy = require('./lib/compiler');
-const tmpFileDir = process.env.TMP_UPLOAD_DIR ? process.env.TMP_UPLOAD_DIR : './tmp';
 
 module.exports = {
   construct: function(self, options) {
 
+    
     /*
     * Create api route for proxying api so we don't have cross origin errors when making AJAX requests
     */
--- packages/cms/lib/modules/app-compiler/lib/compiler.js
+++ packages/cms/lib/modules/app-compiler/lib/compiler.js
@@ -1,29 +1,68 @@
+//const archiver = require('archiver');
+var ncp = require('ncp').ncp;
+ncp.limit = 16;
+
+/**
+ * @param {String} source
+ * @param {String} out
+ * @returns {Promise}
+ */
+function zipDirectory(source, out) {
+  const archive = archiver('zip', { zlib: { level: 9 }});
+  const stream = fs.createWriteStream(out);
+
+  return new Promise((resolve, reject) => {
+    archive
+      .directory(source, false)
+      .on('error', err => reject(err))
+      .pipe(stream)
+    ;
+
+    stream.on('close', () => resolve());
+    archive.finalize();
+  });
+}
+
 /**
  * Compile defined UI to an app
  * buildData : {
-    template: String
+    settings: {
+      appTemplate: String
+    }
  * }
  */
 
 
-const Compiler = (function (buildData) {
-/**
- * 1. Create app build folder
- */
- const tmpDir = os.tmpdir();
- const appDirName = 'app-build-' + (new Date()).getTime();
- const appDir = tmpDir + '/' + appDirName;
+const Compiler = async function (buildData) {
+  /**
+   * 1. Create app build folder
+   */
+   const tmpDir = os.tmpdir();
+   const appDirName = 'app-build-' + (new Date()).getTime();
+   const appDir = tmpDir + '/' + appDirName;
 
 
- if (!fs.existsSync(appDir)){
-   fs.mkdirSync(appDir);
- }
+   if (!fs.existsSync(appDir)){
+     fs.mkdirSync(appDir);
+   }
 
- const appTemplate = buildData.name;
- 
+   const appTemplate = buildData.settings.appTemplate ? buildData.settings.appTemplate : 'clean';
+   const appTemplateDir =
+
+   //copy template to files
+   ncp(appTemplateDir, appDir)
+
+   //create custom files based upon appData
+
+   //layout
+
+   //screens
+
+   //navigation
 
 
-
-})();
+   //output code as zip
+   //zipDirectory();
+};
 
 module.exports = Compiler;
