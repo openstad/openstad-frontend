1aca05a78ee29d96e647aa3e8def480a74c6d632
Tosh Koevoets <tosh.koevoets@gmail.com>
2020-12-17 19:20:31 +0100

    Fixing captcha newsetter

--- packages/cms/lib/modules/openstad-captcha/public/js/main.js
+++ packages/cms/lib/modules/openstad-captcha/public/js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-  $('.refresh-captcha').on('click', function () {
+  // on refresh reload the captcha image
+  $('.captcha-refresh').on('click', function (ev) {
+    ev.preventDefault();
     var src = $('.captcha-img').attr('src');
     var currentTime = new Date().getTime();
     // if multiple refreshes just keep adding, no need to replace
-    src = src.includes('?') ? '&refresh=' + currentTime: '?refresh=' + currentTime;
+    src = src.includes('?') ? src + '&refresh=' + currentTime: src + '?refresh=' + currentTime;
     $('.captcha-img').attr('src', src)
   });
 })
