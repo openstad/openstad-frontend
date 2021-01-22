27ed57fec5ee1580170a9be1e9a2180485b3082d
Tosh Koevoets <tosh.koevoets@gmail.com>
2021-01-22 11:50:12 +0100

    log filepond

--- packages/cms/lib/modules/resource-form-widgets/public/js/main.js
+++ packages/cms/lib/modules/resource-form-widgets/public/js/main.js
@@ -63,6 +63,8 @@ if (fieldsetElement) {
     labelButtonProcessItem: "Upload"
   }
 
+  console.log('filePond', filePond)
+
   var pond = FilePond.create(fieldsetElement, filePondSettings);
 
   var sortableInstance;
