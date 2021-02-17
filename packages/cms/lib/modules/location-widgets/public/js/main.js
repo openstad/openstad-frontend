c4c19b0945e1efb28c65c037098398bea830e8ff
Tosh Koevoets <tosh.koevoets@gmail.com>
2021-02-17 10:17:20 +0100

    update changelog; remove empty console.log

--- packages/cms/lib/modules/location-widgets/public/js/main.js
+++ packages/cms/lib/modules/location-widgets/public/js/main.js
@@ -2,7 +2,6 @@ apos.define('location-widgets', {
     extend: 'map-widgets',
     construct: function(self, options) {
         self.playAfterlibsLoaded = function($widget, data, options) {
-            console.log()
             var map = self.createMap(data.mapConfig);
             self.addPolygon(data.mapConfig);
             self.setIdeaMarker(data.mapConfig);
