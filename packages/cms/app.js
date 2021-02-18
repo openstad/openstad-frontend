7f1edd5f5efee3141ad63c7a9010869ffb716ee6
Tosh Koevoets <tosh.koevoets@gmail.com>
2021-02-18 09:16:27 +0100

    correct way of caching assets

--- packages/cms/app.js
+++ packages/cms/app.js
@@ -50,9 +50,9 @@ const static = express.static('static');
 const aposServer = {};
 
 //todo move this to extension check fo performance
-app.use(express.static('public'), {
+app.use(express.static('public',  {
     maxAge: "1d"
-});
+}));
 // serve static also on first level
 
 app.set('trust proxy', true);
