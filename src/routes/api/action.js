bb54f7f766a3a876de56c071d572b320ae69672a
Tosh Koevoets <tosh.koevoets@gmail.com>
2021-05-24 18:19:32 +0200

    setting up actions and log routes

---
+++ src/routes/api/action.js
@@ -0,0 +1,119 @@
+const Promise = require('bluebird');
+const express = require('express');
+const db      = require('../../db');
+const auth = require('../../middleware/sequelize-authorization-middleware');
+const pagination = require('../../middleware/pagination');
+
+let router = express.Router({mergeParams: true});
+
+router
+    .all('*', function(req, res, next) {
+        req.scope = [];
+        req.scope.push('includeSite');
+        next();
+    });
+
+router.route('/')
+
+    // list Actions
+    // --------------
+    .get(auth.can('Action', 'list'))
+    .get(auth.useReqUser)
+    .get(pagination.init)
+    .get(function(req, res, next) {
+        let { dbQuery } = req;
+
+        req.scope.push({method: ['forSiteId', req.params.siteId]});
+
+        db.Action
+            .scope(...req.scope)
+            .findAndCountAll(dbQuery)
+            .then(result => {
+                req.results = result.rows;
+                req.dbQuery.count = result.count;
+                next();
+            })
+            .catch(next);
+    })
+    .get(auth.useReqUser)
+    .get(pagination.paginateResults)
+    .get(function(req, res, next) {
+        res.json(req.results);
+    })
+
+    // create Action
+    // ---------------
+    .post(auth.can('Action', 'create'))
+    .post(function(req, res, next) {
+        const data = {
+            name   : req.body.name,
+            siteId : req.params.siteId,
+        };
+
+        db.Action
+            .authorizeData(data, 'create', req.user)
+            .create(data)
+            .then(result => {
+                res.json(result);
+            })
+            .catch(next);
+    })
+
+// with one existing Action
+// --------------------------
+router.route('/:actionId(\\d+)')
+    .all(auth.useReqUser)
+    .all(function(req, res, next) {
+        const actionId = parseInt(req.params.actionId);
+        if (!actionId) next('No action id found');
+
+        req.scope = ['defaultScope'];
+        req.scope.push({method: ['forSiteId', req.params.siteId]});
+
+        db.Action
+            .scope(...req.scope)
+            .findOne({ where: { id: actionId } })
+            .then(found => {
+                if ( !found ) throw new Error('Action not found');
+                req.results = found;
+                next();
+            })
+            .catch(next);
+    })
+
+    // view action
+    // -------------
+    .get(auth.can('Action', 'view'))
+    .get(auth.useReqUser)
+    .get(function(req, res, next) {
+        res.json(req.results);
+    })
+
+    // update action
+    // ---------------
+    .put(auth.useReqUser)
+    .put(function(req, res, next) {
+        const action = req.results;
+        if (!( action && action.can && action.can('update') )) return next( new Error('You cannot update this action') );
+        action
+            .authorizeData(req.body, 'update')
+            .update(req.body)
+            .then(result => {
+                res.json(result);
+            })
+            .catch(next);
+    })
+
+    // delete action
+    // ---------------
+    .delete(auth.can('Action', 'delete'))
+    .delete(function(req, res, next) {
+        req.results
+            .destroy()
+            .then(() => {
+                res.json({ "action": "deleted" });
+            })
+            .catch(next);
+    })
+
+module.exports = router;
