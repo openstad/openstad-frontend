a2fbe674b808d08e768f7795680347b95133cbc1
Niels Vegter <niels@denes.nl>
2021-06-24 12:27:08 +0100

    Add react-admin cdn and version automation

--- packages/cms/app.js
+++ packages/cms/app.js
@@ -25,6 +25,7 @@ const path = require('path');
 const morgan = require('morgan')
 
 //internal code
+const cdns = require('./services/cdns');
 const dbExists = require('./services/mongo').dbExists;
 const openstadMap = require('./config/map').default;
 const openstadMapPolygons = require('./config/map').polygons;
@@ -168,39 +169,11 @@ function serveSite(req, res, siteConfig, forceRestart) {
 async function run(id, siteData, options, callback) {
     const site = {_id: id}
 
-    // construct cdn urls
-    let openstadComponentsCdn = process.env.OPENSTAD_COMPONENTS_CDN || 'https://cdn.jsdelivr.net/npm/openstad-components@{version}/dist';
-    if (openstadComponentsCdn.match('{version}')) {
+    let openstadComponentsCdn = await cdns.contructComponentsCdn();
+  let openstadReactAdminCdn = await cdns.contructReactAdminCdn();
+  console.log('xxx', openstadReactAdminCdn);
+    const config = _.merge(siteData, options, { openstadComponentsCdn, openstadReactAdminCdn });
 
-      try {
-        const fs = require('fs').promises;
-        const util = require('util');
-        const exec = util.promisify(require('child_process').exec);
-     
-        let { stdout, stderr } = await exec('git rev-parse --abbrev-ref HEAD');
-        let branch = stdout && stdout.toString();
-
-        let tag = 'alpha';
-        if (branch == 'release') tag = 'beta';
-        if (branch == 'master') tag = 'latest';
-     
-        // get current published version
-        ({ stdout, stderr } = await exec('npm view --json openstad-components'));
-        let info = stdout && stdout.toString();
-        info = JSON.parse(info)
-        let version = info['dist-tags'][tag];
-        if (!version) {
-            // fallback
-            let packageFile = fs.readFileSync(`${__dirname}/package.json`).toString() || '';
-            let match = packageFile && packageFile.match(/"openstad-components":\s*"(?:[^"\d]*)((?:\d+\.)*\d+)"/);
-            version = match && match[1] || null;
-        }
-        openstadComponentsCdn = openstadComponentsCdn.replace('{version}', version);
-      } catch (err) {console.log('Error constructing cdn url', err);}
-
-    }
-
-    const config = _.merge(siteData, options, { openstadComponentsCdn });
     let assetsIdentifier;
 
     // for dev sites grab the assetsIdentifier from the first site in order to share assets
