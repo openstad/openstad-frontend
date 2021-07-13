17134a0944e03f659665224c1bc2af5714f84387
Tosh Koevoets <tosh.koevoets@gmail.com>
2021-07-13 09:11:35 +0200

    Setting up account page

---
+++ packages/cms/lib/modules/openstad-account/index.js
@@ -0,0 +1,24 @@
+/**
+ * This widget load the react-admin library for managing the site settings, and resources in one panel found under /admin
+ * It can be included in a page, has it's own routers.
+ */
+const styleSchema = require('../../../config/styleSchema.js').default;
+
+module.exports = {
+  extend: 'openstad-widgets',
+  label: 'Admin panel',
+  addFields: [
+    //  styleSchema.definition('containerStyles', 'Styles for the container')
+  ],
+  /**
+   * Render account page for user.
+   *
+   * @param self
+   * @param options
+   */
+  construct: function(self, options) {
+    self.apos.app.get('/account', function(req, res) {
+      return self.sendPage(req, '/account');
+    });
+  }
+};
