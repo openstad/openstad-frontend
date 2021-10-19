f0c6266c2904bc9af57f9e16c5cc3e9289db48f0
Niels Vegter <niels@denes.nl>
2021-10-19 10:04:39 +0100

    Update collation and charset in db connector

--- src/db.js
+++ src/db.js
@@ -27,9 +27,10 @@ if (dbConfig.mysqlSTGeoMode || process.env.MYSQL_ST_GEO_MODE === 'on') {
 var sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
 	dialect        : dbConfig.dialect,
 	host           : dbConfig.host,
-	port					: dbConfig.port || 3306,
+	port           : dbConfig.port || 3306,
 	dialectOptions : {
-		charset            : 'utf8_unicode_ci',
+		collation          : 'utf8_unicode_ci',
+		charset            : 'utf8',
 		multipleStatements : dbConfig.multipleStatements,
 		socketPath         : dbConfig.socketPath
 	},
