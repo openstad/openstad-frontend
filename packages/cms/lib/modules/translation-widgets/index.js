cccc9a0ba28a0c1d67831ece85c2d811c95e062f
LorenzoJokhan <lorenzoluk@gmail.com>
2022-10-07 10:56:03 +0200

    Prevent widget from tranlating to NL and just load in original values in node

--- packages/cms/lib/modules/translation-widgets/index.js
+++ packages/cms/lib/modules/translation-widgets/index.js
@@ -1,9 +1,12 @@
 const styleSchema = require('../../../config/styleSchema.js').default;
 const crypto = require('crypto');
 const deepl = require('deepl-node');
-
+const { chunk, result } = require('lodash');
+const translationServiceKey = process.env.TRANSLATION_API_KEY;
+const translator = new deepl.Translator(translationServiceKey);
 const cache = require('../../../services/cache').cache;
 const cacheLifespan  = 8*60*60;   // set lifespan of 8 hours;
+const _ = require('lodash')
 
 module.exports = {
     extend: 'openstad-widgets',
@@ -44,7 +47,7 @@ module.exports = {
                 }
                 widget.cssHelperClassesString = widget.cssHelperClasses ? widget.cssHelperClasses.join(' ') : '';
             });
-
+            
             return superLoad(req, widgets, callback);
         }
 
@@ -62,30 +65,34 @@ module.exports = {
         };
 
         self.route('post', 'submit', function(req, res) {
-            const content = req.body.contents;
-            const contentString = content.join('');
-            const cacheKey = crypto.createHash('sha256').update(contentString).digest('hex');
+            let content = req.body.contents;
+            let referer = req.headers.referer;
+            const destinationLanguage = req.body.targetLanguageCode;
+
+            const cacheKey = crypto.createHash('sha256').update(`${destinationLanguage}${referer}`).digest('hex');
             
             let result = cache.get(cacheKey);
-
             if(result) {
+                console.info("Returning translations from cache");
                 return res.json(result);
             }
-            
-            console.log({content});
 
-            const translationServiceKey = process.env.TRANSLATION_API_KEY;
-            console.log({translationServiceKey});
-            const translator = new deepl.Translator(translationServiceKey);
-            
-            translator.translateText(JSON.stringify(content), req.body.sourceLanguageCode, req.body.targetLanguageCode).then(response => {
-                result = response;
-                console.log({result});
-                return res.json({result});
-                // cache.set(cacheKey, JSON.stringify(result), {
-                //     life: cacheLifespan
-                // })
-            }).catch(error => console.log({error}));
+
+            // There seems to be a bug, when you send a sentence like ", or blablabla" that the translateText function splits on this text, causing an error when running JSON.parse on the translated result, hacky fix
+            const stringifiedContent =  JSON.stringify(content.map(element =>{return element.replace(",", "<k0mma>")}));
+        
+            translator.translateText(
+                stringifiedContent,
+                req.body.sourceLanguageCode,
+                req.body.targetLanguageCode,
+            )
+            .then(response => {
+                cache.set(`${destinationLanguage}${cacheKey}`, response.text, {
+                    life: cacheLifespan
+                })
+                return res.json(response.text);
+            })
+            .catch(error => console.log({error}));
         })
     }
 };
--- packages/cms/lib/modules/translation-widgets/public/js/always.js
+++ packages/cms/lib/modules/translation-widgets/public/js/always.js
@@ -1,51 +1,70 @@
+/** 
+ * Widget responsible for the translation of the page
+ * 
+*/
+
 apos.define('translation-widgets', {
     extend: 'openstad-widgets',
-    construct: function(self, options) {
-      self.play = function($widget, data, options) {
-        $widget.on('click', () => {
-            // fetch content of nodes
-            // send content to api
-            // replace content with translated data
+    construct: function (self, options) {
+        let nodes = [];
+        let nlContents = [];
+        let firstTimeLoading = true;
 
+        $('.translation-widget-select')
+            .on('change', (e) => changeLanguage(e.target.value));
+
+
+        changeLanguage = function (targetLanguageCode) {
+            console.log(`translate to ${targetLanguageCode}`);
             let node = document.body;
-            let toBeTranslated = [];
-            toBeTranslated = handleNode( node, toBeTranslated );
-            
-            let contents = [];
-            toBeTranslated.forEach(itemToTranslate => contents.push(itemToTranslate.orgText));
 
-            let data = JSON.stringify({ contents, sourceLanguageCode: 'nl', targetLanguageCode: 'en-GB' });
-            
-            $.ajax({
-                url: '/modules/translation-widgets/submit',
-                method: 'POST',
-                contentType: "application/json",
-                data,
-                success: function(data) {
-                    console.log('success', data)
-                }
-            })
-        })
-      };
-
-    handleNode = function(node, toBeTranslated) {
-        const childNodes = node.childNodes;
-        for (let i = 0; i < childNodes.length; i++) {
-          if (childNodes[i].nodeType == 1) {
-            let nodeName = childNodes[i].nodeName.toLowerCase();
-            if (nodeName != 'script' && nodeName != 'style')handleNode(childNodes[i], toBeTranslated);
-          } else if (childNodes[i].nodeType == 3) {
-            let textContent = childNodes[i].textContent;
-            textContent = textContent.replace(/^[\s\r\n]+/, '').replace(/[\s\r\n]+$/, '');
-            if ( textContent ) {
-              toBeTranslated.push({
-                node: childNodes[i],
-                orgText: textContent,
-              })
+            if (firstTimeLoading) {
+                nodes = handleNode(node, nodes);
+                nlContents = nodes.map(itemToTranslate => itemToTranslate.orgText);
+                firstTimeLoading = false;
             }
-          }
+
+            if (targetLanguageCode === 'nl') {
+                changeTextInNodes(nlContents);
+            } else {
+                $.ajax({
+                    url: '/modules/translation-widgets/submit',
+                    method: 'POST',
+                    contentType: "application/json",
+                    data: JSON.stringify({ contents: nlContents, sourceLanguageCode: 'nl', targetLanguageCode }),
+                    success: function (sentences) {
+                        changeTextInNodes(JSON.parse(sentences).map(sentence => sentence.replace("<k0mma>", ',')));
+                    }
+                })
+            }
         }
-        return toBeTranslated;
-      }
+
+        changeTextInNodes = function (sentences, postFix = '') {
+            sentences.forEach((sentence, index) => {
+                nodes[index].node.textContent = `${sentence}${postFix}`;
+            });
+        }
+
+        handleNode = function (node, toBeTranslated) {
+            const childNodes = node.childNodes;
+            for (let i = 0; i < childNodes.length; i++) {
+                if (childNodes[i].nodeType == 1) {
+                    let nodeName = childNodes[i].nodeName.toLowerCase();
+                    if (nodeName != 'script' && nodeName != 'style') {
+                        handleNode(childNodes[i], toBeTranslated);
+                    }
+                } else if (childNodes[i].nodeType == 3) {
+                    let textContent = childNodes[i].textContent;
+                    textContent = textContent.replace(/^[\s\r\n]+/, '').replace(/[\s\r\n]+$/, '');
+                    if (textContent) {
+                        toBeTranslated.push({
+                            node: childNodes[i],
+                            orgText: textContent,
+                        })
+                    }
+                }
+            }
+            return toBeTranslated;
+        }
     },
-  });
+});
