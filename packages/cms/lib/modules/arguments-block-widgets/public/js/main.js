9394a3cea09f09905aa85673e95f49417a58d894
LorenzoJokhan <lorenzoluk@gmail.com>
2022-11-09 09:32:31 +0100

    Temporary safety commmit

--- packages/cms/lib/modules/arguments-block-widgets/public/js/main.js
+++ packages/cms/lib/modules/arguments-block-widgets/public/js/main.js
@@ -3,19 +3,22 @@ apos.define('arguments-block-widgets', {
   construct: function (self, options) {
 
     self.play = function (widget, data, options) {
-      loadOpenStadComponents(
-        'reactions',
-        function() {
-          var config = data.config;
-          try {
-            config = JSON.parse(config)
-            config.ideaId = options.activeResourceId || ( options.activeResource && options.activeResource.id ) || false;
-          } catch (err) {}
-          var element = document.querySelector('.'+config.divId);
-          OpenStadComponents['reactions'].Reactions.renderElement(element, config);
-        },
-        data
-      )
+
+      if(options.activeResource.publishDate) {
+        loadOpenStadComponents(
+          'reactions',
+          function () {
+            var config = data.config;
+            try {
+              config = JSON.parse(config)
+              config.ideaId = options.activeResourceId || (options.activeResource && options.activeResource.id) || false;
+            } catch (err) { }
+            var element = document.querySelector('.' + config.divId);
+            OpenStadComponents['reactions'].Reactions.renderElement(element, config);
+          },
+          data
+        )
+      }
     }
   }
 });
