3ee0a6576fc506016281642976b0354f0018c668
LorenzoJokhan <lorenzoluk@gmail.com>
2023-08-31 11:51:02 +0200

    Migrated all of translation widgets code to global

--- packages/cms/lib/modules/openstad-global/public/js/always.js
+++ packages/cms/lib/modules/openstad-global/public/js/always.js
@@ -1,60 +1,131 @@
 apos.on('ready', function () {
+    var firstTimeLoading = true;
     var nodes = [];
-    var selectedLanguage = localStorage.getItem("targetLanguageCode");
+    var nlContents = [];
+
+    // var selectedLanguage = localStorage.getItem("targetLanguageCode");
+    // var toastContainer = document.querySelector("#openstad-toast");
+
+
+    var languageSelectContainer = $('.language-select-container');
+
+    $('.translation-widget-select')
+        .on('change', function (e) { return changeLanguage(e) });
+
+    function setSelectDisabled(select) {
+        select.setAttribute('disabled', true);
+        languageSelectContainer.addClass('languageLoading');
+    };
+
+    function setSelectEnabled(select) {
+        select.removeAttribute('disabled');
+        languageSelectContainer.removeClass('languageLoading');
+    };
+
+    function saveLanguagePreference(targetLanguageCode) {
+        try{
+            localStorage.setItem("targetLanguageCode", targetLanguageCode);
+        } catch(quotaExceededError) {
+            console.log("Could not save the language preference");
+        }
+    }
+
+    function changeLanguage (e) {
+        var select = e.target;
+        var targetLanguageCode = select.value;
+        setSelectDisabled(select);
+
+        
+        var node = document.body;
+
+        if (firstTimeLoading) {
+            nodes = handleNode(node, nodes);
+          nlContents = nodes.map(function(itemToTranslate) { return itemToTranslate.orgText });
+            firstTimeLoading = false;
+        }
+
+        if (targetLanguageCode === 'nl') {
+            console.log("Language is set to the default: " + targetLanguageCode +". No need to translate");
+            changeTextInNodes(nlContents, nodes);
+            setSelectEnabled(select);
+            saveLanguagePreference(targetLanguageCode);
+        } else {
+            console.log('translating to', targetLanguageCode);
+
+            var toastContainer = document.querySelector("#openstad-toast");
+            addToast(toastContainer, "info", "De pagina wordt vertaald...", 5000);
+
+            $.ajax({
+                url: '/modules/translation-widgets/submit',
+                method: 'POST',
+                contentType: "application/json",
+                data: JSON.stringify({
+                    contents: nlContents,
+                    sourceLanguageCode: 'nl',
+                    targetLanguageCode: targetLanguageCode,
+                    origin: window.location.href
+                }),
+                success: function (sentences) {
+                    saveLanguagePreference(targetLanguageCode);
+                    sentences = sentences.map(function(sentence) { return sentence.text });
+                    changeTextInNodes(sentences, nodes);
+                    setSelectEnabled(select);
+                    addToast(toastContainer, "success", "De pagina is succesvol vertaald");
+                }, 
+                error: function() {
+                    setSelectEnabled(select);
+                    setSelectedLanguage('nl');
+                    addToast(toastContainer, "error", "De pagina kon niet worden vertaald");
+                }
+            });
+        }
+    };
+
+
+    function setSelectedLanguage(language) {
+        $('.translation-widget-select').val(language ? language : 'nl').trigger('change');
+    }
+
+
+
+
+    // var selectedLanguage = localStorage.getItem("targetLanguageCode");
+    // var toastContainer = document.querySelector("#openstad-toast");
 
     /** 
      * The translate widget if set on the page will trigger an onchange event when it has been loaded
      * thus triggering the fetching of translations. Then this one should do nothing and let the dedicated 
      * widget make the call.
      */    
-    var translationWidgetOnSamePage = $('.translation-widget-select').length > 0;
+    // var translationWidgetOnSamePage = $('.translation-widget-select').length > 0;
 
-    var userHasSpecialRole = hasModeratorRights; // references global var specified in layout.js'; 
-    var shouldTranslate = shouldTranslateOnEveryPage; // references global var specified in layout.js'; 
-    var toastContainer = document.querySelector("#openstad-toast");
+    // $('.translation-widget-select').on('change', function (e) { startTranslation(nodes, selectedLanguage, toastContainer)});
 
-    if(shouldTranslate) {
-        if (!userHasSpecialRole && !translationWidgetOnSamePage && selectedLanguage && selectedLanguage !== 'nl') {
-            addToast(toastContainer, "info", "De pagina wordt vertaald...", 3000);
-    
-            nodes = handleNode(document.body, nodes);
-            var nlContents = nodes.map(function (itemToTranslate) { return itemToTranslate.orgText });
-            $.ajax({
-                url: '/modules/openstad-global/translate',
-                method: 'post',
-                contentType: "application/json",
-                data: JSON.stringify({
-                    contents: nlContents,
-                    sourceLanguageCode: 'nl',
-                    targetLanguageCode: selectedLanguage,
-                    origin: window.location.href
-                }),
-                success: function (sentences) {
-                    sentences = sentences.map(function (sentence) { return sentence.text });
-                    changeTextInNodes(sentences, nodes);
-                    addToast(toastContainer, "success", "De pagina is succesvol vertaald");
-                },
-                error: function() {
-                    addToast(toastContainer, "error", "De pagina kon niet worden vertaald");
-                }
-            });
-        } else if(userHasSpecialRole) {
-            if(!sessionStorage.getItem("cannot-translate-acknowledged")) {
-                addToast(toastContainer, "info", "De vertaalwidget kan niet worden gebruikt tijdens het bewerken van de site.", 0, function() {
-                    sessionStorage.setItem("cannot-translate-acknowledged", true);
-                });
-            }
-        }
-    }
+
+    // var userHasSpecialRole = hasModeratorRights; // references global var specified in layout.js'; 
+    // var shouldTranslate = false; // references global var specified in layout.js'; 
+
+    // if(shouldTranslate) {
+    //     if (!userHasSpecialRole && !translationWidgetOnSamePage && selectedLanguage && selectedLanguage !== 'nl') {
+    //         startTranslation(nodes, selectedLanguage);
+    //     } else if(userHasSpecialRole) {
+    //         if(!sessionStorage.getItem("cannot-translate-acknowledged")) {
+    //             addToast(toastContainer, "info", "De vertaalwidget kan niet worden gebruikt tijdens het bewerken van de site.", 0, function() {
+    //                 sessionStorage.setItem("cannot-translate-acknowledged", true);
+    //             });
+    //         }
+    //     }
+    // }
 });
 
-changeTextInNodes = function (sentences, nodes) {
+
+function changeTextInNodes (sentences, nodes) {
     sentences.forEach(function (sentence, index) {
         nodes[index].node.textContent = sentence;
     });
 }
 
-handleNode = function (node, toBeTranslated) {
+function handleNode (node, toBeTranslated) {
     var childNodes = node.childNodes;
     for (var i = 0; i < childNodes.length; i++) {
         if (childNodes[i].nodeType == Node.ELEMENT_NODE) {
--- packages/cms/lib/modules/translation-widgets/public/js/always.js
+++ packages/cms/lib/modules/translation-widgets/public/js/always.js
@@ -5,90 +5,90 @@
 apos.define('translation-widgets', {
     extend: 'openstad-widgets',
     construct: function (self, options) {
-        var firstTimeLoading = true;
-        var nodes = [];
-        var nlContents = [];
+        // var firstTimeLoading = true;
+        // var nodes = [];
+        // var nlContents = [];
         
-        var languageSelectContainer = $('.language-select-container');
+        // var languageSelectContainer = $('.language-select-container');
 
-        $('.translation-widget-select')
-            .on('change', function (e) { return changeLanguage(e) });
+        // $('.translation-widget-select')
+        //     .on('change', function (e) { return changeLanguage(e) });
 
-        function setSelectDisabled(select) {
-            select.setAttribute('disabled', true);
-            languageSelectContainer.addClass('languageLoading');
-        };
+        // function setSelectDisabled(select) {
+        //     select.setAttribute('disabled', true);
+        //     languageSelectContainer.addClass('languageLoading');
+        // };
 
-        function setSelectEnabled(select) {
-            select.removeAttribute('disabled');
-            languageSelectContainer.removeClass('languageLoading');
-        };
+        // function setSelectEnabled(select) {
+        //     select.removeAttribute('disabled');
+        //     languageSelectContainer.removeClass('languageLoading');
+        // };
 
-        function saveLanguagePreference(targetLanguageCode) {
-            try{
-                localStorage.setItem("targetLanguageCode", targetLanguageCode);
-            } catch(quotaExceededError) {
-                console.log("Could not save the language preference");
-            }
-        }
+        // function saveLanguagePreference(targetLanguageCode) {
+        //     try{
+        //         localStorage.setItem("targetLanguageCode", targetLanguageCode);
+        //     } catch(quotaExceededError) {
+        //         console.log("Could not save the language preference");
+        //     }
+        // }
 
-        changeLanguage = function (e) {
-            var select = e.target;
-            var targetLanguageCode = select.value;
-            setSelectDisabled(select);
+        // changeLanguage = function (e) {
+        //     var select = e.target;
+        //     var targetLanguageCode = select.value;
+        //     setSelectDisabled(select);
 
             
-            var node = document.body;
+        //     var node = document.body;
 
-            if (firstTimeLoading) {
-                nodes = handleNode(node, nodes);
-              nlContents = nodes.map(function(itemToTranslate) { return itemToTranslate.orgText });
-                firstTimeLoading = false;
-            }
+        //     if (firstTimeLoading) {
+        //         nodes = handleNode(node, nodes);
+        //       nlContents = nodes.map(function(itemToTranslate) { return itemToTranslate.orgText });
+        //         firstTimeLoading = false;
+        //     }
 
-            if (targetLanguageCode === 'nl') {
-                console.log("Language is set to the default: " + targetLanguageCode +". No need to translate");
-                changeTextInNodes(nlContents, nodes);
-                setSelectEnabled(select);
-                saveLanguagePreference(targetLanguageCode);
-            } else {
-                console.log('translating to', targetLanguageCode);
+        //     if (targetLanguageCode === 'nl') {
+        //         console.log("Language is set to the default: " + targetLanguageCode +". No need to translate");
+        //         changeTextInNodes(nlContents, nodes);
+        //         setSelectEnabled(select);
+        //         saveLanguagePreference(targetLanguageCode);
+        //     } else {
+        //         console.log('translating to', targetLanguageCode);
 
-                var toastContainer = document.querySelector("#openstad-toast");
-                addToast(toastContainer, "info", "De pagina wordt vertaald...", 5000);
+        //         var toastContainer = document.querySelector("#openstad-toast");
+        //         addToast(toastContainer, "info", "De pagina wordt vertaald...", 5000);
 
-                $.ajax({
-                    url: '/modules/translation-widgets/submit',
-                    method: 'POST',
-                    contentType: "application/json",
-                    data: JSON.stringify({
-                        contents: nlContents,
-                        sourceLanguageCode: 'nl',
-                        targetLanguageCode: targetLanguageCode,
-                        origin: window.location.href
-                    }),
-                    success: function (sentences) {
-                        saveLanguagePreference(targetLanguageCode);
-                        sentences = sentences.map(function(sentence) { return sentence.text });
-                        changeTextInNodes(sentences, nodes);
-                        setSelectEnabled(select);
-                        addToast(toastContainer, "success", "De pagina is succesvol vertaald");
-                    }, 
-                    error: function() {
-                        setSelectEnabled(select);
-                        setSelectedLanguage('nl');
-                        addToast(toastContainer, "error", "De pagina kon niet worden vertaald");
-                    }
-                });
-            }
-        };
+        //         $.ajax({
+        //             url: '/modules/translation-widgets/submit',
+        //             method: 'POST',
+        //             contentType: "application/json",
+        //             data: JSON.stringify({
+        //                 contents: nlContents,
+        //                 sourceLanguageCode: 'nl',
+        //                 targetLanguageCode: targetLanguageCode,
+        //                 origin: window.location.href
+        //             }),
+        //             success: function (sentences) {
+        //                 saveLanguagePreference(targetLanguageCode);
+        //                 sentences = sentences.map(function(sentence) { return sentence.text });
+        //                 changeTextInNodes(sentences, nodes);
+        //                 setSelectEnabled(select);
+        //                 addToast(toastContainer, "success", "De pagina is succesvol vertaald");
+        //             }, 
+        //             error: function() {
+        //                 setSelectEnabled(select);
+        //                 setSelectedLanguage('nl');
+        //                 addToast(toastContainer, "error", "De pagina kon niet worden vertaald");
+        //             }
+        //         });
+        //     }
+        // };
     }
 });
 
 
-function setSelectedLanguage(language) {
-    $('.translation-widget-select').val(language ? language : 'nl').trigger('change');
-}
+// function setSelectedLanguage(language) {
+//     $('.translation-widget-select').val(language ? language : 'nl').trigger('change');
+// }
 
 /**
  * Makes a call to the backend to translate. This needs to happen to set the initial selection after rendering the page, 
@@ -98,8 +98,8 @@ apos.on('ready', function() {
     var select = document.querySelector('.translation-widget-select');
     var isNormalUser = !hasModeratorRights; // references global var specified in layout.js
     if(isNormalUser) {
-        setSelectedLanguage(localStorage.getItem('targetLanguageCode'));
+        // setSelectedLanguage(localStorage.getItem('targetLanguageCode'));
     } else if(select) {
-        select.setAttribute('disabled', true);
+        // select.setAttribute('disabled', true);
     }
 });
