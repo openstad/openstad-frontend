7c66c2dbc99b01bf1df1be048b048ae69680577a
LorenzoJokhan <lorenzoluk@gmail.com>
2023-11-08 14:59:08 +0100

    For of does not work in uglify

--- packages/cms/lib/modules/resource-form-widgets/public/js/main.js
+++ packages/cms/lib/modules/resource-form-widgets/public/js/main.js
@@ -271,7 +271,10 @@ apos.define('resource-form-widgets', {
         },
         {});
 
-        for (var name of Object.keys(rules)) {
+        var keys = Object.keys(rules);
+
+        for (var i = 0; i < keys.length; i++) {
+          var name = keys[i];
           if (fieldsMadeConceptAware[name]) {
             fieldsMadeConceptAware[name] = Object.assign(
               rules[name],
